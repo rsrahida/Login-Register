29ed472aad5f781e4257d683f6709936814262d5
Rahida Rashidli <reshidlirahide@gmail.com>
2025-02-06 22:16:29 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.querySelector(".clock").innerHTML = h + ":" + m;
+  setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+startTime();
